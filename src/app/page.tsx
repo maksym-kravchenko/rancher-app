b581775f0f6d994bb3ca00dbd18836d5b89aab03
Maksym! <ang.work1607@gmail.com>
2026-05-31 14:40:59 +0200

    chore: init branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,8 @@ export default function Home() {
                 <p className="text-xl text-slate-400 max-w-xl">
                     A Kubernetes portfolio project by Maksym Kravchenko
                     deployed on Rancher with GitOps-style CI/CD.
+
+                    It is a simple test: bla blu bli hahahah
                 </p>
             </div>
 
