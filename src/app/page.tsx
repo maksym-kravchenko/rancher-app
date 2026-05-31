57fea00a779deb4d24a954c986b0269f65c1ec1f
Maksym! <ang.work1607@gmail.com>
2026-05-31 14:25:01 +0200

    feat: tag versioning into ci cd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     return (
         <main className="min-h-screen flex flex-col items-center justify-between p-8 bg-gradient-to-br from-slate-900 via-sky-800 to-slate-900 text-white">
             <div className="flex-1 flex flex-col items-center justify-center text-center">
-                <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-lime-400 via-cyan-700 to-purple-400 bg-clip-text text-transparent">
+                <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-lime-400 to-purple-400 bg-clip-text text-transparent">
                     {message}
                 </h1>
                 <p className="text-xl text-slate-400 max-w-xl">
