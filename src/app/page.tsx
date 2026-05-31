adad6d77accb9f9245f6e729fdbb644069e97922
Maksym! <ang.work1607@gmail.com>
2026-05-31 14:07:35 +0200

    fix: ci cd pipeline 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     const message = process.env.WELCOME_MESSAGE ?? "Hello from local dev";
 
     return (
-        <main className="min-h-screen flex flex-col items-center justify-between p-8 bg-gradient-to-br from-slate-900 via-stone-500 to-slate-900 text-white">
+        <main className="min-h-screen flex flex-col items-center justify-between p-8 bg-gradient-to-br from-slate-900 via-sky-800 to-slate-900 text-white">
             <div className="flex-1 flex flex-col items-center justify-center text-center">
                 <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-lime-400 to-purple-400 bg-clip-text text-transparent">
                     {message}
